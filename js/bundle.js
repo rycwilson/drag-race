cca60cc30a79ed182286933873c24289aa89d0f8
Alex Notov <alex@zealoushacker.com>
2015-01-21 19:10:53 -0800

    Dragster now moving down the track on every right arrow key press

--- js/bundle.js
+++ js/bundle.js
@@ -9,18 +9,44 @@
   RaceTrack         = function() {
     // this class manages the race track
   },
+  Dragster          = function() {
+    // grab the car element
+    this.$el = document.getElementById('dragster');
+    // set the starting position of the dragster
+    this.$el.style.left = "0px";
+  },
   Game              = function() {
     // this class manages game state
     // initialize a christmas tree
     this.tree = new ChristmasTree();
     // initialize the race track
     this.track = new RaceTrack();
+    // initialize the player dragster
+    this.dragster = new Dragster();
+
+    this.attachListeners();
   };
 
   ChristmasTree.prototype.reset = function() {
     // append the css on class to the pre stage lights
     // so that they show up as yellow
     this.$preStageLights.className += " on";
+  };
+
+  Game.prototype.attachListeners = function() {
+    var self = this;
+    // listen for user input, specifically
+    // for the user pressing the right arrow key
+    window.addEventListener('keyup', function(event) {
+      if (event.keyCode === 39) {
+        self.dragster.advance();
+      }
+    });
+  };
+
+  Dragster.prototype.advance = function() {
+    // this should move the car across the screen 1px at a time
+    this.$el.style.left = parseInt(this.$el.style.left, 10) + 1 + "px";
   };
 
   var g = new Game();
